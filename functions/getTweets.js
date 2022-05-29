e759b0cb6ddad61361f057edbff91e34c56cb80a
Adloya <34348778+Adloya@users.noreply.github.com>
2022-05-29 13:15:03 +0200

    Add files via upload

--- functions/getTweets.js
+++ functions/getTweets.js
@@ -1,36 +1,47 @@
-module.exports.getTweets = async () => {
-    let tweetList = [];
-    return {
-        tweets: async function() {
-            const Twitter = require('twitter-lite');
-            const twitterClient = require("../json/twitterclient.json")
-            (async function() {
-                const user = new Twitter({
-                    consumer_key: twitterClient.API_KEY,
-                    consumer_secret: twitterClient.API_KEY_SECRET,
-                });
-            try {
-                    let response = await user.getBearerToken();
-                    const app = new Twitter({
-                        bearer_token: response.access_token,
-                    });
-            // Search for recent tweets
-                    response = await app.get(`/search/tweets`, {
-                        q: "@CLG_Dubois_53", 
-                        lang: "fr",    
-                        count: 100,  
-                    });
-            for (tweet of response.statuses) {
-                        console.dir(tweet.text);
-                    }
-                } catch(e) {
-                    console.log("There was wit the API");
-                    console.dir(e);
-                }
-            })();
-        },
-        get: async function() {
-            return tweetList;
+module.exports.getTweets = async (mode) => {
+    let tweetList = {"error": "empty"};
+    const Twitter = require('twitter-lite');
+    let clientinfo = require("../json/twitterclient.json")
+    const user = new Twitter({
+        consumer_key: clientinfo.API_KEY,
+        consumer_secret: clientinfo.API_KEY_SECRET,
+    });
+    let response = await user.getBearerToken();
+    const app = new Twitter({
+        bearer_token: response.access_token,
+    });
+    let count = 0
+    if(mode == "full"){
+        count = 1000;
+    }else if(mode == "latest"){
+        count = 5;
+    }
+    let tweets = await app.get('statuses/user_timeline', {screen_name: 'CLG_Dubois_53', count: count, tweet_mode: 'extended'});
+    let totaltweets = {}
+    let i = 0;
+    function TweetConstruct(tweets) {
+        while(i < tweets.length){
+            let tweet = {
+                id: tweets[i].id_str,
+                text: tweets[i].full_text,
+                date: tweets[i].created_at,
+                hashtags: tweets[i].entities.hashtags,
+                user_mentions: tweets[i].entities.user_mentions,
+                urls: tweets[i].entities.urls,
+                deviceUsed: tweets[i].source,
+                retweets: tweets[i].retweet_count,
+                likes: tweets[i].favorite_count
+            }
+            totaltweets[i] = tweet;
+            i++
+            if(i == tweets.length){
+                return totaltweets;
+            }
         }
     }
+    TweetConstruct(tweets);
+    tweetList = totaltweets;
+    return {
+        tweetList
+    };
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const news = require('./functions/getNews');
-const twitter = require('./functions/getTweets');
+console.clear()
+const lnews = require('./functions/getLatestNews.js');
+const twitter = require('./functions/getTweets.js');
 
 const fs = require('fs');
 const express = require('express');
@@ -25,82 +26,125 @@ api.get('/', async(req, res) => {
     res.send(jsonres)
 })
 
-api.get('/news', async(req, res) => {
-    // if the news are older than 5 min, update the news in the json file
-    if(await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0] === undefined || await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].lastmodified === 0 || await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].lastmodified === undefined){
-        let n = news.getNews().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
-        await (await n).news().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
-        jsonres.success = true;
-        jsonres.message = "News récupérées avec succès !";
-        jsonres.status = "200";
-        jsonres.source = "site"
-        jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des news"; jsonres.status = "500"; return console.log(err)}));
-        await prisma.latestnews.deleteMany({where: {type: {equals: "news"}}}).catch((err) => console.log(err))
-        prisma.latestnews.create({
-            data: {
-                lastmodified: Date.now(),
-                content: jsonres.content,
-                type: "news"
-            }
-        }).catch((err) => console.log(err))
-        res.send(jsonres)
-    }else if(Date.now() - await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].lastmodified > 1800000) {
-        let n = news.getNews().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
-        await (await n).news().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
-        jsonres.success = true;
-        jsonres.message = "News récupérées avec succès !";
-        jsonres.status = "200";
-        jsonres.source = "site"
-        jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des news"; jsonres.status = "500"; return console.log(err)}));
-        await prisma.latestnews.deleteMany({where: {type: {equals: "news"}}}).catch((err) => console.log(err))
-        prisma.latestnews.create({
-            data: {
-                lastmodified: Date.now(),
-                content: jsonres.content,
-                type: "news"
-            }
-        }).catch((err) => console.log(err))
-        res.send(jsonres);
-    }else{
-        jsonres.success = true;
-        jsonres.message = "News récupérées avec succès !";
-        jsonres.status = "200";
-        jsonres.source = "savefile"
-        jsonres.content = (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].content;
-        res.send(jsonres);
-    }
-})
-
-api.get('/dubnews', async(req, res) => {
+api.get('/latestnews', async(req, res) => {
+    // // if the news are older than 5 min, update the news in the json file
+    // if(await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0] === undefined || await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].lastmodified === 0 || await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].lastmodified === undefined){
+    //     let n = lnews.getNews().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
+    //     await (await n).news().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
+    //     jsonres.success = true;
+    //     jsonres.message = "News récupérées avec succès !";
+    //     jsonres.status = "200";
+    //     jsonres.source = "site"
+    //     jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des news"; jsonres.status = "500"; return console.log(err)}));
+    //     await prisma.latestnews.deleteMany({where: {type: {equals: "news"}}}).catch((err) => console.log(err))
+    //     prisma.latestnews.create({
+    //         data: {
+    //             lastmodified: Date.now(),
+    //             content: jsonres.content,
+    //             type: "news"
+    //         }
+    //     }).catch((err) => console.log(err))
+    //     res.send(jsonres)
+    // }else if(Date.now() - await (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].lastmodified > 1800000) {
+    //     let n = lnews.getNews().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; jsonres.content = {}; jsonres.source = "errorhandler"; return res.send(jsonres)});
+    //     await (await n).news().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; jsonres.content = {}; jsonres.source = "errorhandler"; return res.send(jsonres)});
+    //     jsonres.success = true;
+    //     jsonres.message = "News récupérées avec succès !";
+    //     jsonres.status = "200";
+    //     jsonres.source = "site"
+    //     jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des news"; jsonres.status = "500"; jsonres.content = {}; jsonres.source = "errorhandler"; return console.log(err)}));
+    //     await prisma.latestnews.deleteMany({where: {type: {equals: "news"}}}).catch((err) => console.log(err))
+    //     prisma.latestnews.create({
+    //         data: {
+    //             lastmodified: Date.now(),
+    //             content: jsonres.content,
+    //             type: "news"
+    //         }
+    //     }).catch((err) => console.log(err))
+    //     res.send(jsonres);
+    // }else{
+    //     jsonres.success = true;
+    //     jsonres.message = "News récupérées avec succès !";
+    //     jsonres.status = "200";
+    //     jsonres.source = "savefile"
+    //     jsonres.content = (await prisma.latestnews.findMany({where: {type: {equals: "news"}}}))[0].content;
+    //     res.send(jsonres);
+    // }
     jsonres.success = false;
-    jsonres.message = "La fonctionnalité n'est pas encore disponible";
-    jsonres.status = "500";
-    jsonres.source = "error";
-    jsonres.content = {};
-    res.send(jsonres)
-})
-
-api.get('/twitter', async(req, res) => {
-    let n = twitter.getTweets().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
-    await (await n).tweets().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; jsonres.source = "error"; res.send(jsonres); return console.log(err);});
-    // jsonres.success = true;
-    // jsonres.message = "Tweets récupérés avec succès !";
-    // jsonres.status = "200";
-    // jsonres.source = "twitter";
-    // jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des tweets"; jsonres.status = "500"; jsonres.source = "error"; return console.log(err)}));
-    jsonres.success = false;
-    jsonres.message = "La fonctionnalité n'est pas encore disponible";
+    jsonres.message = "Cette fonctionnalité a été suspendue car elle surchargeait le site du collège";
     jsonres.status = "500";
     jsonres.source = "error";
     jsonres.content = {};
     res.send(jsonres);
 })
 
+api.get('/dubnews', async(req, res) => {
+    jsonres.success = false;
+    jsonres.message = "La fonctionnalité n'est pas encore disponible";
+    jsonres.status = "500";
+    jsonres.source = "errorhandler";
+    jsonres.content = {};
+    res.send(jsonres)
+})
+
+api.get('/twitter', async(req, res) => {
+    // let n = twitter.getTweets().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
+    // await (await n).tweets().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; jsonres.source = "error"; res.send(jsonres); return console.log(err);});
+    jsonres.success = true;
+    jsonres.message = "Tweets récupérés avec succès !";
+    jsonres.status = "200";
+    jsonres.source = "twitter";
+    // jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des news"; jsonres.status = "500"; return console.log(err)}));    
+    // Get tweetlist from getTweets.js
+    let content = (await twitter.getTweets("full")).tweetList;
+    if(content.error){
+        jsonres.success = false;
+        jsonres.message = "Erreur Serveur";
+        jsonres.content = content;
+        jsonres.status = "500";
+        jsonres.source = "errorhandler";
+    }else{
+        jsonres.content = content;
+    }
+    res.send(jsonres)
+})
+
+api.get('/latest_tweets', async(req, res) => {
+    // let n = twitter.getTweets().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; res.send(jsonres)});
+    // await (await n).tweets().catch((err) => {jsonres.success = false; jsonres.message = err; jsonres.status = "500"; jsonres.source = "error"; res.send(jsonres); return console.log(err);});
+    jsonres.success = true;
+    jsonres.message = "Derniers tweets récupérés avec succès !";
+    jsonres.status = "200";
+    jsonres.source = "twitter";
+    // jsonres.content = (await (await n).get().catch((err) => {jsonres.success = false; jsonres.message = "Erreur lors de la récupération des news"; jsonres.status = "500"; return console.log(err)}));    
+    // Get tweetlist from getTweets.js
+    let content = (await twitter.getTweets("latest")).tweetList;
+    if(content.error){
+        jsonres.success = false;
+        jsonres.message = "Erreur Serveur";
+        jsonres.content = content;
+        jsonres.status = "500";
+        jsonres.source = "errorhandler";
+    }else{
+        jsonres.content = content;
+    }
+    res.send(jsonres)
+})
+
 api.get('/youtube', async(req, res) => {
     jsonres.success = false;
     jsonres.message = "La fonctionnalité n'est pas encore disponible";
     jsonres.status = "500";
-    jsonres.source = "error";
+    jsonres.source = "errorhandler";
+    jsonres.content = {};
+    res.send(jsonres)
+})
+
+api.get('/allnews', async(req, res) => {
+    jsonres.success = false;
+    jsonres.message = "La fonctionnalité n'est pas encore disponible";
+    jsonres.status = "500";
+    jsonres.source = "errorhandler";
     jsonres.content = {};
     res.send(jsonres)
 })
@@ -112,7 +156,7 @@ api.use(function(req, res, next){
         jsonres.success = false;
         jsonres.message = "Introuvable";
         jsonres.status = "404";
-        jsonres.source = "error";
+        jsonres.source = "errorhandler";
         res.send(jsonres);
       return;
     }
@@ -120,5 +164,5 @@ api.use(function(req, res, next){
 
 const PORT = process.env.PORT || 3000;
 api.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`API running on port ${ PORT }\nDEBUG: http://localhost:${PORT}/\nPRODUCTION: https://pierredubois-api.herokuapp.com/\n(*) Execution logs :\n`);
 });
